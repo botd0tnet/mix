bc6de346a35b74efe1745186b3f1da38feecd0f5
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-05-18 08:53:27 +0000

    Create mix.sh

---
+++ mix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RED='\033[1;91m' # WARNINGS
+YELLOW='\033[1;93m' # HIGHLIGHTS
+WHITE='\033[1;97m' # LARGER FONT
+LBLUE='\033[1;96m' # HIGHLIGHTS / NUMBERS ...
+LGREEN='\033[1;92m' # SUCCESS
+NOCOLOR='\033[0m' # DEFAULT FONT
+
+dem=0
+
+printf "${LGREEN} Dem Packet Node:" "\n\n"
+for(( i=1; i <=30; i++ ))
+do
+	mix='nym-mixnode'
+	nymmixnode=${mix}${i}
+  printf '%s\n' "${nym}"
+  printf '%s\n' "${i} : $(journalctl -u ${nymmixnode} -o cat | grep "Since startup mixed" | tail -1)"
+done
+printf "\n\n"
+printf "\n\n"
